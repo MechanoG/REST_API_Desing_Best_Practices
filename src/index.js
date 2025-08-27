d701c4ce920582dbeaa4cf3284cb50145fcf46a0
MechanoG <glory_to_the_machine@protonmail.com>
2025-08-26 23:44:19 -0400

    index error correction

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1", v1Router);
-
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, ()=>{
