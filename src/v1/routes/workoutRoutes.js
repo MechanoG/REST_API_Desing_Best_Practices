5fcb6c2173cae54abecaaa070cf242df174696a8
MechanoG <glory_to_the_machine@protonmail.com>
2025-08-27 21:36:42 -0400

    create service layer

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const workoutController = require("../../controllers/workoutController");
+const workoutController = require("../../controllers/workoutControllers");
 
 const router = express.Router();
 
