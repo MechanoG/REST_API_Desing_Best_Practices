50fb28e0c2800ad571e22abeefabdb4a61def28b
MechanoG <glory_to_the_machine@protonmail.com>
2025-09-05 00:54:05 -0400

    recordController

---
+++ src/controllers/recordController.js
@@ -0,0 +1,30 @@
+const recordService = require("../services/recordService");
+
+const getRecordForWorkout = (req, res) => {
+    const {
+        params: workoutId,
+    } = req;
+
+    if(!workoutId){
+        res
+        .status(400)
+        .send({status: "FAILED",
+            data : {error: "Parameter ':workoutId'can not be empty"},
+        })
+    }
+
+    try{
+        const record = recordService.getRecordForWorkout(workoutId);
+        return record;
+    }catch(error){
+        throw{
+            status : error?.status || 500,
+            message : error?.message || error,
+        }
+    }
+
+}
+
+module.exports= {
+    getRecordForWorkout
+}
